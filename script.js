aa26ff27aa1836642892bb6f7581c49bc7ab9edb
Phil <pamramfam@gmail.com>
2023-10-09 10:15:04 -0400

    updated js file with comments

---
+++ script.js
@@ -0,0 +1,15 @@
+// test data 
+// vanilla,vanilla,vanilla,strawberry,coffee,coffee
+
+// Prompt User for flavors 
+
+// Split data into an array 
+
+// declare an empty object to hold flavor counts 
+
+
+// forEach function using flavor 
+
+
+
+// print out data as a table 
